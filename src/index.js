5a45a4e0c4f3b137e1412bb52181e0bcd3d4077e
juliana monteiro <juliana47monteiro@gmail.com>
2021-06-15 14:46:42 -0300

    Primeiro Commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import cipher from './cipher.js';
 
+const cifButton = document.getElementById("cifrarButton");
+    function cifrarMessage () {
+        let valorFaixa = Number(document.getElementById("rangeCifra").value);
+        let textoCaixa = document.getElementById("textCifra").value;
+        const cifrarButton = cipher.encode(valorFaixa, textoCaixa);
+            document.getElementById("resultadoCifra").value = cifrarButton;        
+    }
+    cifButton.addEventListener("click", cifrarMessage);
+
+const decButton = document.getElementById("decifrarButton");
+    function decifrarMessage () {
+        let valorFaixa = Number(document.getElementById("rangeDecifra").value);
+        let textoCaixa = document.getElementById("textDecifra").value;
+        const decifrarButton = cipher.decode(valorFaixa, textoCaixa);
+            document.getElementById("resultadoDecifra").value = decifrarButton;
+        
+    }
+    decButton.addEventListener("click", decifrarMessage);
+
 console.log(cipher);
